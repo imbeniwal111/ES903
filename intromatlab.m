ae5918a88a8e1479497da65f7f27fa5939248237
SANJAY KUMAR BENIWAL <51480008+imbeniwal111@users.noreply.github.com>
2020-08-27 22:05:06 +0530

    Add files via upload

---
+++ intromatlab.m
@@ -0,0 +1,16 @@
+aa = 10;
+b = 12;
+
+% this is commment
+c = a + b;
+
+d = a - b;
+
+e = a*b;
+f = a/b;
+
+% print result
+fprintf('printing results \n')
+fprintf('a = %f; b = %f; \n a + b = %f; \n a-b = %f; \n a*b= %f; \n a/b = %f;\n',a,b,c,d,e,f)
+fprintf('a = %f \n',a )
+fprintf('b = %d \n',b)
